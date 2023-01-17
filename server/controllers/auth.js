7ff85d161d86149a028e0a8369f9daf760dccb0e
maicol <maykolandres4@gmail.com>
2023-01-17 08:07:59 -0500

    done with register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,43 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
+
+
+/** require user   */
+
+export const register = async (req, res) => {
+    try{
+     const {
+        firstName,
+        lastName,
+        email,
+        password,
+        picturePath,
+        friends,
+        location,
+        occupation
+     } = req.body;
+
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: passwordHash,
+        picturePath,
+        friends,
+        location,
+        occupation,
+        viewedProfile: Math.floor(Math.random() * 1000),
+        impressions: Math.floor(Math.random() * 1000)
+      });
+
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+      
+    }catch(err){
+    res.status(500).json({ error: err.message});
+    }
+}
