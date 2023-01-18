c012f715ec35a0d4d7743fcffa2221cf56429647
maicol <maykolandres4@gmail.com>
2023-01-18 07:35:36 -0500

    set reoutes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./layout/homePage";
+import LoginPage from "./layout/loginPage";
+import ProfilePage from "./layout/profilePage";
 
 function App() {
 
 
   return (
-    <div>
-     social media
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/profile/:userid" element={<ProfilePage/>} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
