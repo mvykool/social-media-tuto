f9b6ddca87dd1f3a76a65477b72e01641d524a6a
maicol <maykolandres4@gmail.com>
2023-01-20 05:47:48 -0500

    added some styles ot the login

--- client/src/layout/loginPage/index.tsx
+++ client/src/layout/loginPage/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+
 
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
   return (
-    <div>LoginPage</div>
+ <Box>
+     <Box width="100%" bgcolor={theme.palette.background.default} p="1rem 6%" textAlign="center">
+       <Typography
+        fontWeight="bold"
+        fontSize="clamp(1rem, 2rem, 2.25rem"
+        color="primary">
+          Sociopedia
+        </Typography>
+    </Box>
+    <Box
+    width={isNonMobileScreens ? "50%" : "93%"}
+    p="2rem"
+    m="2rem auto"
+    borderRadius="1.5rem"
+    bgcolor={theme.palette.background.default}
+    >
+      <Typography fontWeight="500" variant="h5" sx={{md: "1.5rem"}}>
+          Welcome to socipedia
+      </Typography>
+
+    </Box>
+ </Box>
   )
 }
 
